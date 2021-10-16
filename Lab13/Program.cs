5040e34459b00e7eb4c791bd49432e98115aa57d
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-16 14:58:12 +0300

    .

--- Lab13/Program.cs
+++ Lab13/Program.cs
@@ -10,6 +10,134 @@ namespace Lab13
     {
         static void Main(string[] args)
         {
+            Building build = new Building("Плесецкая 14", 13, 8, 69);
+            build.Print();
+            Console.ReadKey();
         }
+    }
+
+    class Building
+    {
+        string address;
+        double length;
+        double width;
+        double height;
+
+        public Building(string address, double length, double width, double height)
+        {
+            Address = address;
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public string Address
+        {
+            set 
+            {
+                this.address = value;
+            }
+            get
+            {
+                return address;
+
+            }
+        }
+        
+        public double Length
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    this.length = value;
+                }
+                else
+                {
+                    Console.WriteLine("Длина здания не может быть отрицательной");
+                }
+            }
+            get
+            {
+                return length;
+            }
+        }
+
+        public double Width
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    this.width = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ширина здания не может быть отрицательной");
+                }
+            }
+            get
+            {
+                return width;
+            }
+        }
+
+        public double Height
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    this.height = value;
+                }
+                else
+                {
+                    Console.WriteLine("Высота здания не может быть отрицательной");
+                }
+            }
+            get
+            {
+                return height;
+            }
+        }
+        public void Print()
+        {
+        Console.WriteLine($"Информация о здании:\nАдрес:{address}\nДлина:{length}\nШирина:{width}\nВысота:{height}");
+        }
+    }
+
+    sealed class MultiBuilding : Building
+    {
+        int level;
+
+        public int Level
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    this.level = value;
+                }
+                else
+                {
+                    Console.WriteLine("Этажность здания не может быть отрицательной");
+                }
+            }
+            get
+            {
+                return level;
+            }
+        }
+
+        public MultiBuilding (string address, double length, double width, double height, int level) 
+            :base (address, length, width, height)
+        {
+            Address = address;
+            Length = length;
+            Width = width;
+            Height = height;
+            Level = level;
+        }
+
     }
 }
